edd4a06db154598b4ba0df279a75e372a112d8d3
daniel.kurushin <daniel.kurushin@gmail.com>
2019-11-13 18:32:12 +0500

    interface

--- draw.py
+++ draw.py
@@ -2,7 +2,9 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 import sys
-img = cv.imread(sys.argv[1],0)
+
+from tkinter import *
+img = cv.imread('img/9.jpeg',0)
 edges = cv.Canny(img,100,200)
 contours,hierarchy = cv.findContours(edges, 1, 2)
 
@@ -12,11 +14,24 @@ def d(A, B):
     return ((x1-x0)**2 + (y1-y0)**2)**(1/2)
 
 plines = []
+root = Tk()
+root.bind('<Escape>', lambda x : root.destroy())
+c = Canvas(root, width=640, height=480, bg='white')
+c.pack()
+pen = 'up'
+p = (0,0)
 for a in contours:
-    p = (0,0)
     pline = []
     for b in a:
         for x,y in b:
+            pen = 'up' if d(p, (x,y)) > 20 else 'dn'
+            if pen == 'dn':
+                c.create_line(p[0], p[1], x, y)
+            p = (x,y)
+                
+#print(plines)
+root.mainloop()
+"""
             D = d(p, (x,y))
 #            print(D)
             if 2 < D <= 40:
@@ -26,5 +41,4 @@ for a in contours:
                 plines += [pline]
                 pline = []
             p = (x,y)
-                
-#print(plines)
+"""
