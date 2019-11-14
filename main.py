74243524c0e1123da8ccf84d687cc4e56e04d4c2
daniel kurushin <daniel.kurushin@gmail.com>
2019-11-14 12:31:46 +0500

    interface

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ from tkinter import filedialog as fd
 class KukaWindow(Tk):
     
     def __openfile(self):
-        self.img_name = fd.asksaveasfilename(
+        self.img_name = fd.askopenfilename(
             filetypes=(
-                ("Image files", "*.png;*.jpeg;*.jpg"),
+                ("PNG files", "*.png"),
+                ("JPG files", "*.jpg"),
                 ("All files", "*.*"),
             )
         )
@@ -23,14 +24,24 @@ class KukaWindow(Tk):
         self.bind('<Escape>', lambda x : self.destroy())
 
     def __configure(self):
+        self.A = IntVar()
+        self.B = IntVar()
+        self.A.set(100)
+        self.B.set(200)
+
         self.topframe = Frame(self, height=20)
         self.topframe.pack(side = TOP, expand=1, fill=X)
         self.mainframe = LabelFrame(self, text = 'Изображение', bg='yellow')
         self.mainframe.pack(side = BOTTOM, expand=1, fill=BOTH)
         Button(self.topframe, text='Загрузить файл', command=self.__openfile).pack(side = LEFT)
         Button(self.topframe, text='Экспорт').pack(side = LEFT)
+        Scale(self.topframe, orient=HORIZONTAL, from_ = 0, to = 255, showvalue = 1, variable = self.A, command = self.__startupdate).pack(side = LEFT)
+        Scale(self.topframe, orient=HORIZONTAL, from_ = 0, to = 255, showvalue = 1, variable = self.B, command = self.__startupdate).pack(side = LEFT)
         Canvas(self.mainframe, width=640, height=480, bg='white').pack(fill=BOTH)
         
+    def __startupdate(self, e):
+        print(self.A.get(), self.B.get())
+    
     def __init__(self):
         super().__init__()
         self.__configure()
